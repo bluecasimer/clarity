ebbd94bb37dedccb13a7efd4cec78359406c9cba
Dalmo Cirne <dalmo@clarifai.com>
2018-01-18 18:41:57 -0500

    Showing keyboard

--- Clarity/MainViewController.swift
+++ Clarity/MainViewController.swift
@@ -8,18 +8,39 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    @IBOutlet weak var conceptTextField: UITextField!
     @IBOutlet weak var settingsButton: UIButton!
     @IBOutlet weak var shutterButton: UIButton!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillAppear(_ animated: Bool) {
+        conceptTextField.isHidden = true
+        conceptTextField.alpha = 0
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        NotificationCenter.default.addObserver(self, selector: #selector(MainViewController.handleKeyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(MainViewController.handleKeyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+    }
+
+    // MARK: NotificationHandlers
+    @objc func handleKeyboardWillHide(notification: Notification) {
+        conceptTextField.alpha = 0
+        //        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+        //        }
+    }
+
+    @objc func handleKeyboardWillShow(notification: Notification) {
+        conceptTextField.isHidden = false 
+        UIView.animate(withDuration: 0.4) {
+            self.conceptTextField.alpha = 1
+        }
+//        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+//        }
     }
 
     @IBAction func addConcept(_ sender: AnyObject) {
-        NSLog("Add concept")
+        conceptTextField.becomeFirstResponder();
     }
 }
