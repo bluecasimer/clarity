341617f3929f428cb449bafe2a4ed369b9d5b2de
Dalmo Cirne <dalmo@clarifai.com>
2018-01-18 15:22:16 -0500

    Add shutter button

--- Clarity/MainViewController.swift
+++ Clarity/MainViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    @IBOutlet weak var settingsButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
