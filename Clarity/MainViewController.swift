dffa6be552f622f23456b6bb46808b798e4f66b7
John Patrick Sloan <jpgsloan@gmail.com>
2019-03-11 13:14:25 -0400

    Apply apache license to Clarity (#7)

--- Clarity/MainViewController.swift
+++ Clarity/MainViewController.swift
@@ -2,8 +2,19 @@
 //  MainViewController.swift
 //  Clarity
 //
-//  Copyright © 2018 Clarifai. All rights reserved.
+//  Copyright © 2019 Clarifai
 //
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 
 import UIKit
 import AVFoundation
@@ -122,6 +133,7 @@ class MainViewController: UIViewController, FrameExtractorDelegate, UITableViewD
         }
         isProcessingImage = true;
 
+        // Convert the UIImage to a clarifai Input and predict.
         let dataAsset = DataAsset.init(image: Image.init(image: image))
         let input = Input.init(dataAsset: dataAsset)
         predictWithGeneralModel(input: input)
--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 Hello and welcome! This repository contains the source code of the Clarity app. Here you can see and learn how we used the [Clarifai Apple SDK](https://github.com/Clarifai/clarifai-apple-sdk) to make Clarity do all the cool things it does.
 
-# Getting started
+## Getting started
 
 We are using CocoaPods to integrate the SDK, but in your projects you also have the option of integrating manually.
 
+## License
+
+[Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0). See the LICENSE file for more info.
